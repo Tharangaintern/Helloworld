b4eaf3846d07f2ea2defd2a01aac5c3474f72323
Tharangaintern <52046117+Tharangaintern@users.noreply.github.com>
2022-03-01 17:49:07 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,8 @@
+class Simple{  
+    public static void main(String args[]){  
+    // System.out.println("Hello Java"); 
+for(int i =0;i<10;i++){
+	 System.out.println("Hello Java..."+i); 
+}	 
+    }  
+}  
